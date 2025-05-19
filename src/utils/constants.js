16b019bedb20e30e7d9c273bdfb35318526df97b
sanjai2208 <sanjaiboopathi2005@gmail.com>
2025-05-19 17:05:32 +0530

    ninth commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:2810"
+export const BASE_URL = "/api"
 export const LOGO_URL = "https://img.freepik.com/premium-vector/illustration-web-development-programmer-coding-website_746655-2851.jpg?w=740"
 
